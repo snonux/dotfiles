3172c4039b84ebf8e92b1d419f70c5b583e89f33
Paul Buetow <paul@buetow.org>
2025-09-08 12:26:02 +0300

    Update

--- dotfiles/fish/conf.d/update.fish
+++ dotfiles/fish/conf.d/update.fish
@@ -17,7 +17,7 @@ function update::tools
     go install golang.org/x/tools/cmd/goimports@latest &
     set -a pids $last_pid
 
-    for hexai in hexai hexai-lsp hexai-tmux-action
+    for prog in hexai hexai-lsp hexai-tmux-action
         echo "Installing/updating $prog from codeberg.org/snonux/hexai/cmd/$prog@latest"
         go install codeberg.org/snonux/hexai/cmd/$prog@latest &
         set -a pids $last_pid
