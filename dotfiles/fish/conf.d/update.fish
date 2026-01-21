a881685d948fde1acc40922b8db8d66c258bd131
Paul Buetow <paul@buetow.org>
2026-01-21 22:43:47 +0200

    Update

--- dotfiles/fish/conf.d/update.fish
+++ dotfiles/fish/conf.d/update.fish
@@ -44,7 +44,7 @@ function update::tools
         go install github.com/aandrew-me/tgpt/v2@latest &
         set -a pids $last_pid
 
-        for prog in gos gitsyncer yoga
+        for prog in gos gitsyncer yoga totalrecall
             echo "Installing/updating $prog from codeberg.org/snonux/$prog/cmd/$prog@latest"
             go install codeberg.org/snonux/$prog/cmd/$prog@latest
         end
