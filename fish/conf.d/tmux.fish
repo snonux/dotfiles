72b6ca7c2bf84b7f4f7a065384fc566457ddd4cf
Paul Buetow <paul@buetow.org>
2026-03-08 08:34:54 +0200

    Update

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -57,7 +57,7 @@ function tmux::project
     if not test -f $TMUX_FZF_GIT_INDEX
         find $git_dir -maxdepth 4 -type d -name .git \
             | sed 's|/.git$||' | sed "s|$git_dir/||" \
-            | grep -F -v . | grep -v gitsyncer-workdir >$TMUX_FZF_GIT_INDEX
+            | grep -F -v . | grep -v gitsyncer-workdir | grep -v upstream >$TMUX_FZF_GIT_INDEX
     end
 
     set -l matches (grep "$filter" $TMUX_FZF_GIT_INDEX)
