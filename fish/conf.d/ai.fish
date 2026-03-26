6ec4c79f9f0f45c242012955effc3099188745e8
Paul Buetow <paul@buetow.org>
2026-03-26 23:10:50 +0200

    fish: source ask completions from ~/go/bin/ask

--- fish/conf.d/ai.fish
+++ fish/conf.d/ai.fish
@@ -5,8 +5,10 @@ if test -f ~/git/hypr/hypr.fish
     source ~/git/hypr/hypr.fish
 end
 
-if test -f ~/git/hexai/assets/ask.fish
-    source ~/git/hexai/assets/ask.fish
+set -l ask_bin ~/go/bin/ask
+
+if test -x $ask_bin
+    $ask_bin fish | source
 else
-    echo No ~/git/hexai/assets/ask.fish found
+    echo No $ask_bin found
 end
