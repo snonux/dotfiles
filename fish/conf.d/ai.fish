7a396075e9a4cef1441192177dacaf20b771c259
Paul Buetow <paul@buetow.org>
2026-03-26 22:09:44 +0200

    Update

--- fish/conf.d/ai.fish
+++ fish/conf.d/ai.fish
@@ -4,3 +4,9 @@ abbr -a explain 'hexai explain'
 if test -f ~/git/hypr/hypr.fish
     source ~/git/hypr/hypr.fish
 end
+
+if test -f ~/git/hexai/assets/ask.fish
+    source ~/git/hexai/assets/ask.fish
+else
+    echo No ~/git/hexai/assets/ask.fish found
+end
