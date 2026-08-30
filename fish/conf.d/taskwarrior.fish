59d72021e0ec07177f9472fcfcdd44eaed40cebb
Paul Buetow <paul@buetow.org>
2026-08-30 16:34:27 +0300

    Update

--- fish/conf.d/taskwarrior.fish
+++ fish/conf.d/taskwarrior.fish
@@ -505,6 +505,7 @@ abbr -a ts tasksamurai due.before:today+7d
 abbr tpt taskwarrior::project_tasks
 abbr tsp taskwarrior::project_tasks::tasksamurai
 abbr st 'supersync; tasksamurai due.before:today+7d'
+abbr agenttasks tasksamurai +agent
 abbr agentasks tasksamurai +agent
 
 taskwarrior::due_count
