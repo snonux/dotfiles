67c9e6c6f2b7ef6e218063bf6fd810f64c1a72c0
Paul Buetow <paul@buetow.org>
2026-08-31 20:26:16 +0300

    Update

--- fish/conf.d/worktime.fish
+++ fish/conf.d/worktime.fish
@@ -4,102 +4,6 @@ if test (uname) = Darwin -a ! -f ~/.wtloggedin
     echo "Warn: Not logged in, run wtlogin"
 end
 
-# timesamurai is now the system of record for time tracking. worktime.rb
-# (the old Ruby tracker) is kept only for manual access via the `old`-suffixed
-# commands below (wtloginold, wtlogoutold, etc.) -- it is no longer wired into
-# the day-to-day wtlogin/wtlogout/wtadd/wtreport/wtstatus/wtedit commands and
-# nothing mirrors between the two stores anymore.
-function worktime::old
-    ruby $WORKTIME_DIR/worktime.rb $argv
-end
-
-function worktime::old::add
-    set -l seconds $argv[1]
-    set -l what $argv[2]
-    set -l descr $argv[3]
-    set -l epoch (date +%s)
-
-    if test -z "$what"
-        set what work
-    end
-
-    if test -z "$descr"
-        worktime::old --add $seconds --epoch $epoch --what $what
-    else
-        worktime::old --add $seconds --epoch $epoch --what $what --descr "$descr"
-    end
-
-    worktime::old::report
-end
-
-# BROKEN, and left broken on purpose: `--log` is ambiguous between --login and
-# --logout, so worktime.rb rejects it and this function has never recorded
-# anything. $seconds is read but never passed, which suggests it was meant to
-# be a copy of worktime::old::add. Guessing at the intent would silently start
-# writing time entries, so it is flagged here for a human to decide instead.
-function worktime::old::log
-    set -l seconds $argv[1]
-    set -l what $argv[2]
-    set -l epoch (date +%s)
-
-    if test -z "$what"
-        set what work
-    end
-
-    worktime::old --log --epoch $epoch --what $what
-    worktime::old::report
-end
-
-function worktime::old::login
-    set -l what $argv[1]
-    if test -z "$what"
-        set what work
-    end
-    touch ~/.wtloggedin
-    worktime::old --login --what $what
-    worktime::wisdom_reminder
-end
-
-function worktime::old::logout
-    set -l what $argv[1]
-
-    if test -z "$what"
-        set what work
-    end
-
-    if test -f ~/.wtloggedin
-        rm ~/.wtloggedin
-    end
-
-    worktime::old --logout --what $what
-    worktime::old::report
-end
-
-function worktime::old::report
-    if test -f ~/.wtloggedin
-        if test -f ~/.wtmaster
-            worktime::old --report | tee $WORKTIME_DIR/report.txt
-        else
-            worktime::old --report
-        end
-        worktime::wisdom_reminder
-    end
-end
-
-function worktime::old::status
-    worktime::old::report
-
-    if test -f ~/.wtloggedin
-        echo "You are logged in"
-        set -l num_worklog (ls $WORKTIME_DIR | grep wl- | wc -l)
-        if test $num_worklog -gt 0
-            echo "$num_worklog entries in the worklog in $WORKTIME_DIR/wl-*"
-        end
-    else
-        echo "You are not logged in"
-    end
-end
-
 function worktime::add
     set -l seconds $argv[1]
     set -l what $argv[2]
@@ -242,17 +146,6 @@ abbr -a wtlogout 'worktime::logout'
 abbr -a wtstatus 'worktime::status'
 abbr -a wtsync 'worktime::sync'
 abbr -a wtf 'timesamurai work report'
-
-# Old system (worktime.rb) -- kept around for manual/legacy access only.
-abbr -a wtold 'worktime::old'
-abbr -a wteditold 'worktime::old --edit'
-abbr -a wtreportold 'worktime::old::report'
-abbr -a wtaddold 'worktime::old::add'
-abbr -a wtlogold 'worktime::old::log'
-abbr -a wtloginold 'worktime::old::login'
-abbr -a wtlogoutold 'worktime::old::logout'
-abbr -a wtstatusold 'worktime::old::status'
-abbr -a wtfold 'worktime::old --report'
 
 abbr -a wl 'task add +work'
 abbr -a ql 'task add +personal'
